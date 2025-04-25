91aa4c805fb4e47e5f0cd30d1c04082f101a83bb
bojja guru mahendra <bojjagurumahendra002@gmail.com>
2025-04-25 11:43:10 +0530

    fourth commit

---
+++ App.js
@@ -0,0 +1,56 @@
+const productApiUrl = 'https://fakestoreapi.com/products';
+let allProducts = [];
+
+async function fetchProducts() {
+  try {
+	const response = await fetch(productApiUrl);
+	allProducts = await response.json();
+	displayProducts(allProducts);
+  } catch (error) {
+	console.error('Error fetching products:', error);
+  }
+}
+
+function displayProducts(products) {
+  const productList = document.getElementById('product-list');
+  productList.innerHTML = '';
+
+  products.forEach(product => {
+	const shortTitle = product.title.length > 40 
+? product.title.substring(0, 40) + "..." 
+: product.title;
+
+const shortDescription = product.description.length > 80 
+? product.description.substring(0, 80) + "..." 
+: product.description;
+
+
+	const productCard = document.createElement('div');
+	productCard.className = 'col-md-4 mb-4';
+
+	productCard.innerHTML = `
+	  <div class="card h-100">
+		<img src="${product.image}" class="card-img-top p-3" alt="${product.title}" style="height: 300px; object-fit: contain;">
+		<div class="card-body">
+		  <h5 class="card-title">${shortTitle}</h5>
+		  <p class="card-text text-center py-2">${shortDescription}</p>
+		  <p class="card-text border text-center py-2">$${product.price}</p>
+		  <button class="btn btn-dark">Details</button>
+		  <button class="btn btn-dark">Add to Cart</button>
+		</div>
+	  </div>
+	`;
+	productList.appendChild(productCard);
+  });
+}
+
+function filterByCategory(category) {
+  if (category === 'all') {
+	displayProducts(allProducts);
+  } else {
+	const filtered = allProducts.filter(product => product.category === category);
+	displayProducts(filtered);
+  }
+}
+
+fetchProducts();
